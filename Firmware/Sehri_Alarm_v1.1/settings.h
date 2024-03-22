f94fd9104120c6604f8cbc179ff00f0d91e444e0
shanto1803170 <nhshanto1999@gmail.com>
2024-03-22 19:33:18 +0600

    Update settings.h

--- Firmware/Sehri_Alarm_v1.1/settings.h
+++ Firmware/Sehri_Alarm_v1.1/settings.h
@@ -1,10 +1,10 @@
 // microSD Card Reader connections
-#define SD_CS          4
+#define SD_CS          5
 #define SPI_MOSI      23 
 #define SPI_MISO      19
 #define SPI_SCK       18
  
 // I2S Connections
-#define I2S_DOUT      27
-#define I2S_BCLK      25
-#define I2S_LRC       26
+#define I2S_DOUT      25
+#define I2S_BCLK      26
+#define I2S_LRC       27
